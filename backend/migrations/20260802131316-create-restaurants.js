7facd5521fdb922e186d343a6707df808a534dbd
Kannan <kannans02001@gmail.com>
2026-08-02 19:21:04 +0530

    feat: database config and restaurant migration

---
+++ backend/migrations/20260802131316-create-restaurants.js
@@ -0,0 +1,44 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable("Restaurants", {
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false,
+      },
+
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+
+      address: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+
+      contact: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+
+      createdAt: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+
+      updatedAt: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+    });
+  },
+
+  async down(queryInterface) {
+    await queryInterface.dropTable("Restaurants");
+  },
+};
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,7 +1,22 @@
 import app from "./app";
+import { sequelize } from "./config/database";
+
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+const STARTSERVER = async () => {
+  try {
+    await sequelize.authenticate();
+    console.log('Database connection successful');
+    app.listen(PORT, () => {
+      console.log(`Server running on http://localhost:${PORT}`);
+    })
+  } catch (error) {
+    console.error("Database connection failure.");
+    console.error(error);
+
+    process.exit(1);
+  }
+}
+
+STARTSERVER();
