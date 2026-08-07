ca249561a820611e2b3a4af494139f6aafdabd78
Kannan <kannans02001@gmail.com>
2026-08-07 20:15:46 +0530

    estaurant sequelize model

---
+++ backend/src/infrastructure/database/models/restaurant.model.ts
@@ -0,0 +1,44 @@
+import { DataTypes, Model, Sequelize } from "sequelize";
+
+export interface IRestaurant {
+     id?: number;
+     name: string;
+     address: string;
+     contact: string;
+}
+
+export class Restaurant extends Model<IRestaurant> implements IRestaurant {
+     public id!: number;
+     public name!: string;
+     public address!: string;
+     public contact!: string;
+}
+
+export const initRestaurantModel = (sequelize: Sequelize) => {
+     Restaurant.init(
+          {
+               id: {
+                    type: DataTypes.INTEGER,
+                    primaryKey: true,
+                    autoIncrement: true,
+               },
+               name: {
+                    type: DataTypes.STRING,
+                    allowNull: false,
+               },
+               address: {
+                    type: DataTypes.STRING,
+                    allowNull: false,
+               },
+               contact: {
+                    type: DataTypes.STRING,
+                    allowNull: false,
+               },
+          },
+          {
+               sequelize,
+               tableName: "Restaurants",
+               timestamps: true,
+          }
+     );
+};
